42ff41cea02572a99df62c507e4a2b2f56001c96
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-20 08:58:43 +0530

    [Ganapati]Stack Push

---
+++ StackandQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace StackandQueue
+{
+    public class program
+    {
+
+        public static void Main(string[] args)
+        {
+            Stack_Ex stackobj = new Stack_Ex();
+            stackobj.Push(20);
+            stackobj.Push(30);
+            stackobj.Push(40);
+            stackobj.Display();
+        }
+    }
+
+
+
+}
---
+++ StackandQueue/Stack_Ex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    public class Stack_Ex
+    {
+        Node top;
+        
+        public Stack_Ex()
+        {
+            top = null;
+        }
+
+        public void Push(int data)
+        {
+            Node newnode=new Node(data);
+            newnode.Next = top;
+            top = newnode;
+
+        }
+        public void Display()
+        {
+            if(top==null)
+            {
+                Console.WriteLine("Stack id Emoty");
+            }
+            Node temp = top;
+            while(temp!=null)
+            {
+                Console.WriteLine(" "+temp.data+" ");
+                temp = temp.Next;
+
+            }
+        }
+    }
+}
